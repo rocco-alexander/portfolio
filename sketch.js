3ad23d022542ca3147ed6b0a2da01e6988f1e389
rocco-alexander <roccoalexander@hotmail.com>
2022-08-19 16:51:37 -0400

    attempting autoplay on iOS

--- sketch.js
+++ sketch.js
@@ -2,63 +2,60 @@
 // CODE TRAIN 
 
 let vid;
-let playing = true;
 let sourceText;
 let words;
 let startIndex = 0;
-let asciiDiv;
+
 
 function preload() {
     vid = loadImage("rocco32.jpg")
     sourceText = loadStrings("skillsList.txt");
-    vid = loadImage("ezgif-5-53e35b7cee.gif")
-    vid = loadImage("ezgif-5-8d880efd64.gif")
+    // vid = loadImage("ezgif-5-53e35b7cee.gif")
+    // vid = loadImage("ezgif-5-8d880efd64.gif")
 }
 
 function setup() {
     let canvas = createCanvas(350, 350);
     canvas.parent("ascii-container")
-    vid= createVideo('video.mp4')
+    // vid= createVideo('video.mp4')
     words = sourceText.join(" ");
 
-    
-  vid.size(48 ,48);
-  vid.volume(0);
-  vid.loop();
-  vid.hide(); // hides the html video loader
-  vid.autoplay(true)
-  vid.play()
+//   vid.size(48 ,48);
+//   vid.volume(0);
+//   vid.loop();
+//   vid.hide(); // hides the html video loader
+//   vid.autoplay(true)
+//   vid.play()
   textFont("Arial");
 }
 
 function draw() {
     frameRate(10);
-    // background(0)
+    // background(1)
     let charIndex = startIndex;
     let w = width / vid.width;
     let h = height / vid.height;
+
     vid.loadPixels()
-    // image(vid)
-
-for (let j = 0; j < vid.height; j++){
-    for (let i = 0 ; i <vid.width; i++ ){
-        const pixelIndex = (i + j * vid.width) * 4;
-        const r = vid.pixels[pixelIndex + 0]
-        const g = vid.pixels[pixelIndex + 1]
-        const b = vid.pixels[pixelIndex + 2]
-        const avg = (r + g + b) / 2
-
-        noStroke()
-        fill(avg)
-        textSize(w * 1.1);
-        textAlign(CENTER,CENTER)
-        text(
-            words.charAt(charIndex % words.length),
-            i * w + w * 0.5,
-            j * h + h * 0.5
-        );
-        charIndex++
-    }
-}
-// startIndex++
+    
+    for (let j = 0; j < vid.height; j++){
+        for (let i = 0 ; i <vid.width; i++ ){
+            const pixelIndex = (i + j * vid.width) * 4;
+            const r = vid.pixels[pixelIndex + 0] 
+            const g = vid.pixels[pixelIndex + 1]
+            const b = vid.pixels[pixelIndex + 2]
+            let avg = (r + g + b) / 2
+            noStroke()
+            fill(avg)
+            textSize(w * 4)
+            textAlign(CENTER,CENTER)
+            text(
+                words.charAt(charIndex % words.length),
+                i * w + w * 0.5,
+                j * h + h * 0.5
+                );
+                charIndex++
+            }
+        }
+        startIndex++
 }
